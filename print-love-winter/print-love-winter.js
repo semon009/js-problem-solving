0905777fafedf7c37d98246e7ffeb6379f233a0c
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-09 21:15:12 +0300

    Create print-love-winter.js

---
+++ print-love-winter/print-love-winter.js
@@ -0,0 +1,15 @@
+/**
+ * Problem: Print Multi-line Text
+ * 
+ * Description:
+ * Write a program that prints the following text on separate lines:
+ * I
+ * love
+ * winter
+ * 
+ * Expected Output:
+ * I
+ * love
+ * winter
+ */
+console.log("I\nlove\nwinter");
