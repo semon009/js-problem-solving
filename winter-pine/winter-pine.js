b81b6b4818c9cd353ad6172b031216ef8381bc39
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-09 21:08:05 +0300

    Add winter pine tree pattern program
    Implement a program to print a winter pine tree pattern.

---
+++ winter-pine/winter-pine.js
@@ -0,0 +1,13 @@
+/**
+ * Winter Pine Pattern
+ * 
+ * Description:
+ * Write a program that prints a winter pine tree using '#' and '.' symbols.
+ * 
+ * Expected Output:
+ * ...#...
+ * ..###..
+ * .#####.
+ * ...#...
+ */
+console.log("...#...\n..###..\n.#####.\n...#...");
