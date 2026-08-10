c8b4b73cdad02f0f99ac726e36bc4295473956a2
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-10 15:02:14 +0300

    Add script to print variable values to console
    This script prints the values of the variables 'name' and 'age' to the console.

---
+++ Print Variables/print-variables.js
@@ -0,0 +1,18 @@
+/**
+ * Problem: Print Variables
+ * 
+ * Description:
+ * Print all variables defined in the given code ('name' and 'age') into the console.
+ * 
+ * Expected Output:
+ * Jessica
+ * 26
+ */
+
+// Variable declarations
+const name = "Jessica";
+let age = 26;
+
+// Solution: Outputting variable values to the console
+console.log(name);
+console.log(age);
