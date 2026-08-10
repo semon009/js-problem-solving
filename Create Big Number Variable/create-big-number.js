197a8320d700bde88133e745cbd4f71781103fa9
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-10 15:21:24 +0300

    Create bigNumber variable initialized to 9999
    Create a constant variable 'bigNumber' initialized to 9999 and log its value.

---
+++ Create Big Number Variable/create-big-number.js
@@ -0,0 +1,16 @@
+/**
+ * Problem: Create Big Number Variable
+ * 
+ * Description:
+ * Create the bigNumber variable and set it to 9999.
+ * 
+ * Expected Output:
+ * 9999
+ */
+
+// Solution: Declaring a constant variable initialized with a numeric value
+const bigNumber = 9999;
+// or you can use 'let'
+
+// Execute and log value
+console.log(bigNumber);
