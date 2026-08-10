edf4713c256fb99f62ae04dcad44e6d95db50b70
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-10 16:18:44 +0300

    Create typeof-number-555.js

---
+++ 555/typeof-number-555.js
@@ -0,0 +1,13 @@
+/**
+ * Problem: Output Data Type of 555
+ * 
+ * Description:
+ * Using the typeof operator, output the data type of the number 555 to the console.
+ * 
+ * Expected Output:
+ * number
+ */
+
+// Solution: Assign 555 to a variable and check its type using the typeof operator
+let Five_Hundred_Fifty_Five = 555;
+console.log(typeof(Five_Hundred_Fifty_Five));
