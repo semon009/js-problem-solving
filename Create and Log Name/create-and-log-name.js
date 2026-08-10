b12675a25519616b00227332e24dcaa1fad71200
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-10 16:00:32 +0300

    Create create-and-log-name.js

---
+++ Create and Log Name/create-and-log-name.js
@@ -0,0 +1,16 @@
+/**
+ * Problem: Create and Log Name Variable
+ * 
+ * Description:
+ * Create a variable 'name' with your name and log it to the console. 
+ * The name must start with a capital Latin letter.
+ * 
+ * Expected Output:
+ * Wpizza67676767
+ */
+
+// Solution: Declare a string starting with a capital Latin letter and log it
+const name = "Wpizza67676767";
+
+// Execute and log to console
+console.log(name);
