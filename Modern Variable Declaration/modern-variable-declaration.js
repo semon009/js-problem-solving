22a95e0f4cafad5076b244e3f283038e32361f02
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-10 15:15:07 +0300

    Rewrite variable declaration using modern syntax
    Replaced outdated 'var' declaration with 'const' for fixed value.

---
+++ Modern Variable Declaration/modern-variable-declaration.js
@@ -0,0 +1,17 @@
+/**
+ * Problem: Rewrite Outdated Variable Declaration
+ * 
+ * Description:
+ * Rewrite the outdated 'var' variable declaration using modern JavaScript keywords ('const' or 'let').
+ * 
+ * Code to rewrite:
+ * var name = "Catherine";
+ */
+
+// Solution using 'const' (preferred since the value is fixed)
+const name = "Catherine";
+
+// Alternative solution using 'let' (if 'name' needs to be reassigned later):
+// let name = "Catherine";
+
+console.log(name);
