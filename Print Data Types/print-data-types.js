f5cc2538feb95556c7b1f9123dcce19e891b307d
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-10 16:12:10 +0300

    Create print-data-types.js

---
+++ Print Data Types/print-data-types.js
@@ -0,0 +1,17 @@
+/**
+ * Problem: Print Data Types
+ * 
+ * Description:
+ * Write code that prints the data type of the following three values to the console:
+ * "an apple", 63, and 19.3.
+ * 
+ * Expected Output:
+ * string
+ * number
+ * number
+ */
+
+// Solution: Using the typeof operator to check data types
+console.log(typeof("an apple"));
+console.log(typeof(63));
+console.log(typeof(19.3));
