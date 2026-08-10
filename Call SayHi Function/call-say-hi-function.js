6ce396d4a380969e823010cf86614e717dfc2c3e
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-10 19:27:42 +0300

    Create call-say-hi-function.js

---
+++ Call SayHi Function/call-say-hi-function.js
@@ -0,0 +1,16 @@
+/**
+ * Problem: Call SayHi Function
+ * 
+ * Description:
+ * Call the existing 'sayHi' function to print "Hello" to the console.
+ * 
+ * Expected Output:
+ * Hello
+ */
+
+function sayHi() {
+  console.log("Hello");
+}
+
+// Solution: Invoke/Call the sayHi function
+sayHi();
