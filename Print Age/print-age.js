585697a88f042a52626f9fd50e65c6c918254237
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-10 16:23:49 +0300

    Add print-age.js to log age to console
    Implement a script to print a specified age.

---
+++ Print Age/print-age.js
@@ -0,0 +1,15 @@
+/**
+ * Problem: Print Age
+ * 
+ * Description:
+ * Print the number corresponding to your age in the console.
+ * 
+ * Expected Output:
+ * 9942394959239693
+ */
+
+// Solution: Store age in a variable and output it to the console
+let age = 9942394959239693;
+
+// Log value to console
+console.log(age);
