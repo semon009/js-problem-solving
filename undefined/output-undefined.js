a1b1c2c539c7eafe63bf8927ace230612b899aa4
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-11 12:57:14 +0300

    Create output-undefined.js

---
+++ undefined/output-undefined.js
@@ -0,0 +1,18 @@
+/**
+ * Problem: Output Undefined to Console
+ * 
+ * Description:
+ * Change the code so that calling getData outputs 'undefined' to the console.
+ * 
+ * Expected Output:
+ * undefined
+ */
+
+// Solution: Define 'data' variable without initialization and call getData() with no arguments
+let data;
+
+function getData(data) {
+  console.log(data);
+}
+
+getData();
