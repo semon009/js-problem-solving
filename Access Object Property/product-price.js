e829d9ff9c9c580cbef02a299a947cc61871ecf7
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-11 11:28:47 +0300

    Create product-price.js

---
+++ Access Object Property/product-price.js
@@ -0,0 +1,17 @@
+/**
+ * Problem: Print Product Price
+ * 
+ * Description:
+ * Access and print the 'price' property of the 'product' object to the console.
+ * 
+ * Expected Output:
+ * 48.7
+ */
+
+let product = {
+  name: "Smart Clock",
+  price: 48.7
+};
+
+// Solution: Access property using dot notation
+console.log(product.price);
