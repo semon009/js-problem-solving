97959fcb9035fe032cbe40f6bb252f3efa577ba2
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-11 11:34:18 +0300

    Create robot object with specified properties
    Create a robot object with model, year, and canDance properties.

---
+++ Create Robot Object/create-robot-object.js
@@ -0,0 +1,26 @@
+/**
+ * Problem: Create Robot Object
+ * 
+ * Description:
+ * Create an object named 'robot' with the following properties:
+ * - 'model': "RX9000"
+ * - 'year': 2024
+ * - 'canDance': true
+ * 
+ * Expected Output:
+ * RX9000
+ * 2024
+ * true
+ */
+
+// Solution: Create object literal with specified properties
+let robot = {
+  model: "RX9000",
+  year: 2024,
+  canDance: true
+};
+
+// Log properties to console
+console.log(robot.model);
+console.log(robot.year);
+console.log(robot.canDance);
