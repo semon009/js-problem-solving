a503d3729a77a6d42ba41bfebe12d0cff17082f2
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-12 14:44:39 +0300

    Create say-hi-function.js

---
+++ sayHi/say-hi-function.js
@@ -0,0 +1,17 @@
+/**
+ * Problem: Say Hi Function
+ * 
+ * Description:
+ * Create the 'sayHi' function that outputs "Hello" to the console.
+ * 
+ * Expected Output:
+ * Hello
+ */
+
+// Solution: Define function that logs 'Hello'
+function sayHi() {
+  console.log("Hello");
+}
+
+// Execute function
+sayHi();
