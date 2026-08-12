d7cbd6aad83fdf44cbd6af1c17f27b6eaa74ca04
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-12 19:11:18 +0300

    Create create-anonymous-function.js

---
+++ Create Anonymous Function/create-anonymous-function.js
@@ -0,0 +1,18 @@
+/**
+ * Problem: Create Anonymous Function
+ * 
+ * Description:
+ * Create an anonymous function that returns the number 10 and 
+ * assigns it to the 'createNumber' const.
+ * 
+ * Expected Output:
+ * 10
+ */
+
+// Solution: Assign an anonymous function expression to a constant
+const createNumber = function () {
+  return 10;
+};
+
+// Execute and log result
+console.log(createNumber());
