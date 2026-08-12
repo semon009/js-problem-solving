706f3ad34571807c8f0d10519ddf7cd9455eff31
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-12 14:51:29 +0300

    Create sum-function.js

---
+++ Sum Function/sum-function.js
@@ -0,0 +1,18 @@
+/**
+ * Problem: Sum Function
+ * 
+ * Description:
+ * Create the 'sum' function that takes two numbers as parameters and returns their sum.
+ * 
+ * Example:
+ * sum(3, 5) -> returns 8
+ * sum(10, 20) -> returns 30
+ */
+
+// Solution: Function that accepts two parameters and returns their sum
+function sum(x, y) {
+  return x + y;
+}
+
+// Execute and log result
+console.log(sum(3, 5));
