e6b7753f9d3536f10dd7fada9767eb5230368fbd
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-12 14:34:55 +0300

    Create returnNumber function to return a number
    Implemented a function that returns the input number.

---
+++ Return Number Function/return-number-function.js
@@ -0,0 +1,18 @@
+/**
+ * Problem: Return Number Function
+ * 
+ * Description:
+ * Create the 'returnNumber' function that takes a number as a parameter and returns it.
+ * 
+ * Example:
+ * returnNumber(5) -> returns 5
+ * returnNumber(42) -> returns 42
+ */
+
+// Solution: Function that accepts a number parameter and returns it
+function returnNumber(x) {
+  return x;
+}
+
+// Execute and log result
+console.log(returnNumber(5));
