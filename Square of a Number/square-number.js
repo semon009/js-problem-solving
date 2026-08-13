bbc1e2e5ab45a43e120e6adb5d7b27863b73fba5
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-13 13:32:22 +0300

    Create square-number.js

---
+++ Square of a Number/square-number.js
@@ -0,0 +1,17 @@
+/**
+ * Problem: Square of a Number
+ * 
+ * Description:
+ * Create an arrow function named 'square' that takes a single number 
+ * as input and returns the square of that number.
+ * 
+ * Sample Input 1: 5 -> Expected Output 1: 25
+ * Sample Input 2: 0 -> Expected Output 2: 0
+ */
+
+// Solution: Arrow function calculating the square of num
+const square = (num) => num * num;
+
+// Testing the solution
+console.log(square(5)); // Outputs: 25
+console.log(square(0)); // Outputs: 0
