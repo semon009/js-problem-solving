4da27b6d2d7c565849b1fcdda9686a01d97fba7a
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-15 14:29:16 +0300

    Declare two Boolean variables with initial values

---
+++ Declare Boolean Variables/declare-boolean-variables.js
@@ -0,0 +1,15 @@
+/**
+ * Problem: Declare Boolean Variables
+ * 
+ * Description:
+ * Declare a variable called 'bool1' and set its value to true. 
+ * Declare a second variable named 'bool2' and set its value to false.
+ */
+
+// Solution:
+let bool1 = true;
+let bool2 = false;
+
+// Testing the solution
+console.log("bool1:", bool1); // Outputs: bool1: true
+console.log("bool2:", bool2); // Outputs: bool2: false
