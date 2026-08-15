a0c1e1128718170c894fd3790b1b564540c2747b
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-15 17:44:10 +0300

    Add pub age checker functionality
    Implement pub age checker to evaluate entry eligibility based on age.

---
+++ Pub Age Checker/pub-age-checker.js
@@ -0,0 +1,17 @@
+/**
+ * Problem: Pub Age Checker
+ * 
+ * Description:
+ * Check if a person is allowed to enter a pub based on their age.
+ * Set 'age' to 18 and evaluate whether 'age' is less than 18.
+ */
+
+// Solution:
+let age = 18;
+let condition = age < 18;
+
+if (condition) {
+    console.log("Sorry, but you are a minor.");
+} else {
+    console.log("That's ok. You can enter the pub.");
+}
