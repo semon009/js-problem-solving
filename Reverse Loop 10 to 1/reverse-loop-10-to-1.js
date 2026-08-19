6734ca79071d308053dd39b6002c04700fa11f0c
Simon009 <109869861+semon009@users.noreply.github.com>
2026-08-19 18:54:18 +0300

    Create reverse-loop-10-to-1.js

---
+++ Reverse Loop 10 to 1/reverse-loop-10-to-1.js
@@ -0,0 +1,23 @@
+/**
+ * Problem: Display Numbers 10 to 1 in Reverse Order
+ * 
+ * Description:
+ * Create a loop that logs all numbers from 10 down to 1 in the console.
+ * 
+ * Expected Output:
+ * 10
+ * 9
+ * 8
+ * 7
+ * 6
+ * 5
+ * 4
+ * 3
+ * 2
+ * 1
+ */
+
+// Solution: Standard for loop with decrementing counter
+for (let i = 10; i > 0; i--) {
+    console.log(i);
+}
